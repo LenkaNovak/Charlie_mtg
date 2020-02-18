7b1019b498b522dd9a303c2ee7aa6cd34437aeb1
zeptodoctor <44736852+zeptodoctor@users.noreply.github.com>
2020-02-18 19:30:28 +0000

    build based on 63f0e62

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"#Charlie_mtg-1","page":"Home","title":"Charlie_mtg","text":"","category":"section"},{"location":"#","page":"Home","title":"Home","text":"This is my Charlie_mtg package","category":"page"}]
+}
